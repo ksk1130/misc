a69c455b2966dc812455af30f6629a992922a160
Sasaki Keisuke <ksk.andr@gmail.com>
2022-08-18 09:40:41 +0900

    リポジトリを移動(recursive_sql_test)

---
+++ recursive_sql_test/create_table_sales_details.sql
@@ -0,0 +1,28 @@
+create table sales_details(
+    sales_ym varchar(6) not null
+   ,fruit_code varchar(4) not null
+   ,sales_volume int
+);
+
+insert into sales_details values('202004','0001',10);
+insert into sales_details values('202004','0002', 8);
+insert into sales_details values('202004','0003', 4);
+insert into sales_details values('202005','0001',12);
+insert into sales_details values('202005','0002', 6);
+insert into sales_details values('202005','0003', 5);
+insert into sales_details values('202006','0001', 4);
+insert into sales_details values('202006','0002',16);
+insert into sales_details values('202006','0003',10);
+insert into sales_details values('202007','0001', 4);
+insert into sales_details values('202007','0002',16);
+insert into sales_details values('202007','0003',10);
+insert into sales_details values('202008','0100', 5);
+insert into sales_details values('202008','0002',13);
+insert into sales_details values('202008','0003',12);
+insert into sales_details values('202009','0101', 7);
+insert into sales_details values('202009','0002', 9);
+insert into sales_details values('202009','0003',10);
+insert into sales_details values('202010','0101', 2);
+insert into sales_details values('202010','0002', 3);
+insert into sales_details values('202010','0003', 4);
+
