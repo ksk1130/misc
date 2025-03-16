73e96bbfadc4dffd55e27c10f38192a9e6edc61a
Sasaki Keisuke <ksk.andr@gmail.com>
2025-03-16 11:45:14 +0900

    コメントを追記

--- merge_git_repos/merge_git_repos.sh
+++ merge_git_repos/merge_git_repos.sh
@@ -6,6 +6,9 @@ REMOTE_REPO_NAME=natgw_onoff
 TARGET_REPO_PATH=~/natgw_onoff
 DEFAULT_BRANCH_NAME=master
 
+# このコマンド群では、あるリモートリポジトリの指定したブランチを、指定したディレクトリに取り込みます。
+# git read-tree : 指定したリモートリポジトリの指定したブランチを、指定したディレクトリに取り込む
+# git merge -s subtree : 指定したリモートリポジトリの指定したブランチを、指定したディレクトリに取り込む
 git remote add $REMOTE_REPO_NAME $TARGET_REPO_PATH \
 && git fetch $REMOTE_REPO_NAME \
 && git read-tree --prefix=$REMOTE_REPO_NAME/ $REMOTE_REPO_NAME/$DEFAULT_BRANCH_NAME \
